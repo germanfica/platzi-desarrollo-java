351c740691c4c17ff31de88e64ab1f12ed74b963
German Fica <germafica@gmail.com>
2020-07-26 14:33:27 -0300

    Ejercicios Platzi (consignas)

---
+++ src/com/williambluish/test/TestFamily.java
@@ -0,0 +1,12 @@
+package com.williambluish.test;
+
+/**
+ * Variables
+ *
+ * Ejercicio 1.
+ * Ponte creativo y preséntanos a tu familia con variables, utiliza todos los tipos y cantidad
+ * de variables que puedas, no olvides las convenciones de nombres Camel Case.
+ */
+public class TestFamily {
+
+}
